495f283d3eb6fcfecf96866f5a52e2b5fc6a26ca
Maxim Kott <maximkott@gmail.com>
2015-11-10 19:26:58 +0100

    Refactoring AlphaConstraint.

--- src/Weew/Validator/Constraints/AlphaConstraint.php
+++ src/Weew/Validator/Constraints/AlphaConstraint.php
@@ -6,9 +6,6 @@ use Weew\Validator\IConstraint;
 
 /**
  * Check if the value consists of alphabetical characters.
- * A character is considered as alphabetical if its a letter from current locale
- * Default is a-z A-Z
- * http://php.net/manual/en/function.setlocale.php
  */
 class AlphaConstraint implements IConstraint {
     /**
@@ -17,7 +14,11 @@ class AlphaConstraint implements IConstraint {
      * @return bool
      */
     public function check($value) {
-        return ctype_alpha($value);
+        if (is_string($value)) {
+            return preg_match('/[[:alpha:]]/u', $value) === 1;
+        }
+
+        return false;
     }
 
     /**
